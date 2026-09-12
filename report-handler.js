5a523193f0a11fbf9c4fc819bb3d889e83f5f9be
vxhienvn <andrmarvn@gmail.com>
2026-09-12 21:35:17 +0700

    fix(v10): install Core admin compatibility routes before legacy UIs

--- report-handler.js
+++ report-handler.js
@@ -1,1 +1,8 @@
-export { installReportRoutes } from "./report-handler-v11.js";
+import { installReportRoutes as installV11ReportRoutes } from "./report-handler-v11.js";
+import { installV10CoreAdminCompat } from "./v10-core-admin-compat.js";
+
+export function installReportRoutes(app, options = {}) {
+  const result = installV11ReportRoutes(app, options);
+  installV10CoreAdminCompat(app, options);
+  return result;
+}
