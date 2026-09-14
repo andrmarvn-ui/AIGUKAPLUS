c41261d94a6cee7dc1ef86e077959b005c41d264
vxhienvn <andrmarvn@gmail.com>
2026-09-14 13:11:22 +0700

    fix(v10): prioritize authenticated bridge admin routes

--- report-handler.js
+++ report-handler.js
@@ -1,7 +1,9 @@
 import { installReportRoutes as installV11ReportRoutes } from "./report-handler-v11.js";
 import { installV10CoreAdminCompat } from "./v10-core-admin-compat.js";
+import { installV10BridgeAdminRoutes } from "./v10-bridge-admin-routes.js";
 
 export function installReportRoutes(app, options = {}) {
+  installV10BridgeAdminRoutes(app, options);
   const result = installV11ReportRoutes(app, options);
   installV10CoreAdminCompat(app, options);
   return result;
