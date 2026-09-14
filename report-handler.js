d93761650ed519d7b9983188f71c666b8d18c910
vxhienvn <andrmarvn@gmail.com>
2026-09-14 13:20:39 +0700

    fix(v10): use Core proxy admin routes as authoritative bridge

--- report-handler.js
+++ report-handler.js
@@ -1,10 +1,8 @@
 import { installReportRoutes as installV11ReportRoutes } from "./report-handler-v11.js";
 import { installV10CoreAdminCompat } from "./v10-core-admin-compat.js";
 import { installV10BridgeAdminRoutes } from "./v10-bridge-admin-routes.js";
-import { installV10EdgeAdminRoutes } from "./v10-edge-admin-routes.js";
 
 export function installReportRoutes(app, options = {}) {
-  installV10EdgeAdminRoutes(app, options);
   installV10BridgeAdminRoutes(app, options);
   const result = installV11ReportRoutes(app, options);
   installV10CoreAdminCompat(app, options);
