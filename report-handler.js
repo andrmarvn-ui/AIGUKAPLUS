86aff5d4cdd70b978d4883422b9ba0b1da5e3154
vxhienvn <andrmarvn@gmail.com>
2026-09-17 10:27:12 +0700

    fix(v10): install Core Drive mapping and slide routes before legacy handlers

--- report-handler.js
+++ report-handler.js
@@ -1,9 +1,11 @@
 import { installReportRoutes as installV11ReportRoutes } from "./report-handler-v11.js";
 import { installV10CoreAdminCompat } from "./v10-core-admin-compat.js";
 import { installV10BridgeAdminRoutes } from "./v10-bridge-admin-routes.js";
+import { installV10DriveMappingCompat } from "./v10-drive-mapping-compat.js";
 
 export function installReportRoutes(app, options = {}) {
   installV10BridgeAdminRoutes(app, options);
+  installV10DriveMappingCompat(app, options);
   const result = installV11ReportRoutes(app, options);
   installV10CoreAdminCompat(app, options);
   return result;
